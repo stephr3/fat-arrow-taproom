e07acc830d34375341342bfdc9c970c91001d819
Noah Yasskin and Stephanie Gurung <noah@>
2016-08-09 14:07:46 -0700

    add subtractPint function with working button

--- app/keg.component.ts
+++ app/keg.component.ts
@@ -6,8 +6,12 @@ import { Keg } from './keg.model';
   inputs: ['keg'],
   template: `
     <h3>"{{ keg.name }}", Brand: {{ keg.brand }}, Style: {{ keg.style }}, Price: \${{ keg.price }}, Alcohol: {{ keg.alcohol }}%, Pints#: {{ keg.pints }}</h3>
+    <button (click)="subtractPint(keg)" class="btn btn-success btn-lg"> - </button>
   `
 })
 export class KegComponent {
   public keg: Keg;
+  subtractPint(subtractKeg: Keg): void {
+  subtractKeg.pints = subtractKeg.pints - 1;
+  }
 }
