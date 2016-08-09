9f1d565d7bb1b476c8f78a0215b9e6229581f07f
Noah Yasskin and Stephanie Gurung <noah+stephanie@>
2016-08-09 09:58:54 -0700

    log a keg object on click

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from 'angular2/core';
   template: `
     <div class="container">
       <h1>Fat Arrow => Taproom</h1>
-      <h3 *ngFor="#keg of kegs">{{ keg.name }}, Brand: {{ keg.brand }}, Style: {{ keg.style }}, Price: {{ keg.price }}, Alcohol: {{ keg.alcohol }}, Pints: {{ keg.pints }}</h3>
+      <h3 *ngFor="#keg of kegs" (click)="kegWasSelected(keg)">{{ keg.name }}, Brand: {{ keg.brand }}, Style: {{ keg.style }}, Price: {{ keg.price }}, Alcohol: {{ keg.alcohol }}, Pints: {{ keg.pints }}</h3>
     </div>
   `
 })
@@ -16,10 +16,13 @@ export class AppComponent {
     this.kegs = [
       new Keg("Stabby Lambda", "Ruby", "Saison", 7, 5.5, 0),
       new Keg("Hash Rocket", "Ruby", "IPA", 4, 8, 1),
-      new Keg("Moustache Curly Braces", "Angular", "Amber Red", 4, 4, 2),
+      new Keg("Mustache Curly Braces", "Angular", "Amber Red", 4, 4, 2),
       new Keg("args!", "Angular", "Stout", 6, 7, 3),
       new Keg("*ngFor", "Angular", "Lager", 3, 3, 4)
     ];
+  }
+  kegWasSelected(clickedKeg: Keg): void {
+    console.log(clickedKeg);
   }
 }
 
